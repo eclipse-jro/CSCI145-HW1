b3fec22d37147f3b7857667ca91169c24c56666a
J. Rene Ortega Jr <atlasbusinessmarketing@gmail.com>
2019-01-23 10:10:54 -0800

    Added a few parameters to the monster class and added a main method

--- src/Monster.java
+++ src/Monster.java
@@ -1,3 +1,8 @@
 public class Monster {
-    //code
+    private int hp;
+    private int gp;
+    private int dmg;
+
+    //public int getHp() { return hp; }
+    public  void setHp(int hp) { this.hp = hp; }
 }
