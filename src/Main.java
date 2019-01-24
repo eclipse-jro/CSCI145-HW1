17aa0de33e44134aeca878d7cd03ee90dfff0af6
J. Rene Ortega Jr <atlasbusinessmarketing@gmail.com>
2019-01-23 20:56:04 -0800

    Merged Jons forked branch to the main repro, added GameConstants.java, added to Player Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args){
-        //our main method
+    public static void main(String[] args) {
+
+        // player test 1; hp, gp, damage
+        Player p1 = new Player(3, 6, 9);
+
+        //monster test 2
+        Player p2 = new Player(2, 4, 8);
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,43 @@
 public class Player {
-    //code
+    //fields
+    private int health;
+    private int gold;
+    private int damage;
+    private String playerType;
+    private String entranceText;
+
+    // constructor for easier initialization of Player objects;
+    // random player type method implemented per Jon's example.
+    public Player(int h, int g, int d){
+        RandomGenerator generator = new RandomGenerator();
+        int playerRand = generator.randInt(1,2);
+
+        switch(playerRand){
+            case 1:
+                playerType = "Thief";
+                health = GameConstants.THIEF_INITAL_HEALTH;
+                damage = GameConstants.THIEF_DAMAGE;
+                entranceText =
+                        "As a drunk thief, you awake after a long night of drinking.\n"
+                        "It is time for the daily usual; stealing and running from the witty to stay afloat."
+                break;
+            case 2:
+                playerType = "Warrior";
+                health = GameConstants.WARRIOR_INITAL_HEALTH;
+                damage = GameConstants.WARRIOR_DAMAGE;
+                entranceText =
+                        "You awake startled. It appears you have lost your battle last night.\n"
+                        "As a warrior your instincts kick in, you sense a dangerous presence and smell GOLD."
+                break;
+        }
+        /* Not sure if switch or below will be better for creating multiple objects of the same class / type.
+        this.health = h;
+        this.gold = g;
+        this.damage = d;
+        */
+    }
+    // getter (accessor method)
+    public String getEntranceText() {
+        return entranceText;
+    }
 }
