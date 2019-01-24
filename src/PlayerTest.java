f5b808081a6e57db8ce389c3a4091bcadec02e72
J. Rene Ortega Jr <atlasbusinessmarketing@gmail.com>
2019-01-24 15:31:38 -0800

    added PlayerTest class

---
+++ src/PlayerTest.java
@@ -0,0 +1,6 @@
+public class PlayerTest {
+    public void runTest1(){
+        // not sure if we should create a customer constructor yet / what is most efficient
+        //Player player = new Player();
+    }
+}
