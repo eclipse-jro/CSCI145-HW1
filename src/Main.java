64fbb654c0577e731f1fcd439932772e5c4fbced
joncgarber <garberdude.jg@gmail.com>
2019-01-24 16:55:24 -0800

    added testing sequence in main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+    	
         // player test 1; hp, gp, damage
         Player p1 = new Player(3, 6, 9);
 
         //monster test 2
-        Player p2 = new Player(2, 4, 8);
+        Scanner input = new Scanner(System.in);
+        int damageDealt;
+        int damageTaken = -1;
+        
+        Monster testMonster = new Monster();
+        System.out.println(testMonster.getEntranceText());
+        System.out.println();
+        while(testMonster.isDefeated() == false && damageTaken != 0) {
+        	damageDealt = testMonster.attack(); 
+        	System.out.println("The " + testMonster.getType() + " attacks you for "
+        					+ damageDealt + " damage!");
+        	System.out.print("How much damage do you deal?: ");
+        	damageTaken = input.nextInt();
+        	if (damageTaken != 0) {
+        		testMonster.onHit(damageTaken);
+        		System.out.println("The " + testMonster.getType() +
+        				" has " + testMonster.getHealth() + " health.");
+        	}
+        	else {
+        		System.out.println("You run away!");
+        	}
+        	
+        }
+        if(testMonster.isDefeated()) {
+        	System.out.println(testMonster.getDeathText());
+        }
+        else {
+        	System.out.println(testMonster.getEscapeText());
+        }
+        
     }
 }
