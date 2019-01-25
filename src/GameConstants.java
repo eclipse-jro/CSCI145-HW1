9d9990a928b4dab4adaf320674d3508cffdf7c71
joncgarber <garberdude.jg@gmail.com>
2019-01-24 16:43:51 -0800

    minor updates to constants

--- src/GameConstants.java
+++ src/GameConstants.java
@@ -1,25 +1,27 @@
 public class GameConstants {
 
     // Thief Player Class Constants
-    public static final int THIEF_INITAL_HEALTH = 70;
+    public static final int THIEF_INITIAL_HEALTH = 70;
     public static final int THIEF_DAMAGE = 10;
+    public static final double THIEF_GOLD_MULTIPLIER = 1.2;
     // public static final int THIEF_GOLD = 20%; not sure how we will use this yet.
 
     // Warrior Player Class Constants
-    public static final int WARRIOR_INITAL_HEALTH = 100;
+    public static final int WARRIOR_INIITAL_HEALTH = 100;
     public static final int WARRIOR_DAMAGE = 15;
+    public static final double WARRIOR_GOLD_MULTIPLIER = 1.0;
     // public static final int WARRIOR_GOLD = normal; not sure how we will use this yet.
 
     // Deneke Monster Constants
-    public static final int DENEKE_INITAL_HEALTH = 55;
+    public static final int DENEKE_INITIAL_HEALTH = 55;
     public static final int DENEKE_MAX_DAMAGE = 5;
     // Goblin Monster Constants
-    public static final int GOBLIN_INITAL_HEALTH = 6;
+    public static final int GOBLIN_INITIAL_HEALTH = 6;
     public static final int GOBLIN_MAX_DAMAGE = 10;
     // Orc Monster Constants
-    public static final int ORC_INITAL_HEALTH = 18;
+    public static final int ORC_INITIAL_HEALTH = 18;
     public static final int ORC_MAX_DAMAGE = 20;
     // Zombie Monster Constants
-    public static final int ZOMBIE_INITAL_HEALTH = 12;
+    public static final int ZOMBIE_INITIAL_HEALTH = 12;
     public static final int ZOMBIE_MAX_DAMAGE = 15;
 }
